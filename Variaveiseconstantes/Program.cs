05fff8d47cbf737403da720a0369cce6c3ba41a1
KEVIN PEREIRA SILVA <kevin.psilva3@senacsp.edu.br>
2025-10-30 17:05:31 -0300

    aula 30-10

---
+++ Variaveiseconstantes/Program.cs
@@ -0,0 +1,40 @@
+﻿//Variáveis
+char letra = 'a';
+string nome = "joão";
+float temperatura = 36.5f;
+double salario = 12345.67;
+bool estaChovendo = true;
+
+// Alterando as variáveis
+letra = 'b';
+nome = "Maria";
+temperatura = 37.0f;
+salario = 15000.50;
+estaChovendo = false;
+
+
+//Constantes
+const char simbolo = '$';
+const string saudacao = "bem vindo ao c#";
+const float pi = 3.14159f;
+const double altura = 1.75;
+const bool ehVerdade = false;
+const int numero = 14;
+
+
+//Exibindo os vlores originais por interpolação
+Console.WriteLine("Valores originais:");
+Console.WriteLine($"Letra: {letra}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Temperatura: {temperatura}");
+Console.WriteLine($"Salário: {salario}");
+Console.WriteLine($"Está chovendo? {estaChovendo}");
+
+//Exibindo os valores originais por concantenação
+Console.WriteLine("\nConstantes:");
+Console.WriteLine("Símbolo: " + simbolo);
+Console.WriteLine("Saudação: " + saudacao);
+Console.WriteLine("Valor de pi: " +  pi);
+Console.WriteLine("Altura: " +  altura);
+Console.WriteLine("É verdade? " + ehVerdade);
+Console.WriteLine("Numero: " +  numero);
---
+++ matematica/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//declarando constantes 
+const int valor1 = 10;
+const int valor2 = 5;
+
+
+//soma
+int soma = valor1 + valor2;
+Console.WriteLine($"soma: {valor1} + {valor2} = {soma}");
+
+//subtração
+int subtracao = valor1 - valor2;
+Console.WriteLine($"subtração: {valor1} - {valor2} = {subtracao}");
+
+//Multiplicação 
+int multiplicacao = valor1 * valor2;
+Console.WriteLine($"Multiplicação: {valor1} * {valor2} = {multiplicacao}");
+
+//Divisão (resultado)
+int divisao  = valor1 / valor2;
+Console.WriteLine($"divisão: {valor1} / {valor2} = {divisao}");
+
+int restoDivisao = valor1 % valor2;
+Console.WriteLine($"Resto de divisão de: {valor1} / {valor2} = {restoDivisao}");
+
+//potenciação (usando Math.pow para calcular a potência)
+double potencial = Math.Pow(valor1, valor2);
+Console.WriteLine($"potenciação: {valor1} elevado a {valor2} = {potencial}");
+
+//Exemplo 1: Sem parêmteses 
+int resultado1 = 3 + 5 * 2;
+Console.WriteLine($"Resultado 1 (sem parênteses): 3 + 5 * 2 = {resultado1}");
+
+//Explo 1: com parênteses
+int resultado2 = (3 + 5) * 2;
+Console.WriteLine($"Resultado 2 (com parênteses): (3 + 5) * 2 = {resultado2}");
+
+//Explo 2: sem parênteses
+int resultado3 = 8 / 4 + 3;
+Console.WriteLine($"Resultado 3 (com parênteses): 8 / 4 + 3 = {resultado3}");
+
+//Explo 4: com parênteses
+double resultado4 = 8.0 / (4 + 3);
+Console.WriteLine($"Resultado 4 (com parênteses): 8 / (4 + 3) = {resultado4}");
