7d2c305d1193b29c4f0418c801270db8d89769dc
KEVIN PEREIRA SILVA <kevin.psilva3@senacsp.edu.br>
2025-11-06 15:03:54 -0300

    enviando atualização

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿// Operadores Lógicos
+bool x = true;
+bool y = false;
+
+bool and = x && y; //E lógico
+bool or = x || y; // ou lógico
+bool not = !x; //Não lógico 
+
+Console.WriteLine("\nOperadores Lógicos");
+Console.WriteLine($"x && y: {and}");
+Console.WriteLine($"x || y: {or}");
+Console.WriteLine($"!x: {not}");
---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 10;
+int b = 5;
+
+//Operadores Relacionais 
+bool maiorQue = a > b;
+bool menorQue  = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuigual = a >= b;
+bool menorOuigual = a  <= b;
+
+//Exibindo resultados dos operadores relacionais 
+Console.WriteLine("\nOperadores Relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorQue}");
+Console.WriteLine($"{a} < {b}: {menorQue}");
+Console.WriteLine($"{a} == {b}: {menorQue}");
+Console.WriteLine($"{a} != {b}: {igual}");
+Console.WriteLine($"{a} >=b {b}: {maiorOuigual}");
+Console.WriteLine($"{a} <= {b}: {menorOuigual}");
