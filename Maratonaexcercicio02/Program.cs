3c3d6324336032988a35dadb9b8892f184561382
KEVIN PEREIRA SILVA <kevin.psilva3@senacsp.edu.br>
2025-11-18 17:10:12 -0300

    18-11

---
+++ Maratonaexcercicio02/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Faça um programa para verificação de estoque
+Um sistema de gerenciamento de estoque precisa notificar os funcionários
+quando for hora de reabastecer um produto.
+Escreva um código que verifique a quantidade de um produto em estoque. Se a
+quantidade for menor que 5, exiba a mensagem: "Alerta: Baixo estoque. Por favor,
+reabasteça este produto.". Caso contrário, exiba: "Estoque suficiente.".*/
+
+
+using System.ComponentModel.Design;
+
+int parse = 5;
+{
+    Console.WriteLine("Digite aquantidade de numero no estoque");
+        int quantidade = (int)parse;((Console.ReadLine());
+    {
+        if (quantidade < 5)
+        {
+            Console.WriteLine("Alerta: Baixo estoque. por favor, reabasteça este produto");
+        }
+    
---
+++ Vetoresematrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿//Declaração de dois vetores 
+int[] numeros = { 7, 2, 3, 34, 5 }; //Vetor de inteiros
+string[] nomes = { "Ana", "João", "Maria", "Carlos" }; //Vetor de string 
+
+//Exibindo os elementos do vetor de numeros com loop for 
+Console.WriteLine("Vetor de numeros (usando for:");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+// Exibindo os elementos do vetor de nomes com loop foreach 
+Console.WriteLine("\nVetor de nomes (usando foreach):");
+foreach (var nome  in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+//Ordenação de vetores 
+Array.Sort(numeros);
+Console.WriteLine("\nVetor de Números Ordenado:");
+foreach (var n in numeros)
+{
+    Console.WriteLine(n);
+}
+
+int valorProcurado = 5;
+int posicaoSequencial = PesquisaSequencial(numeros, valorProcurado);
+
+if (posicaoSequencial != -1)
+{
+    Console.WriteLine($"\nPesquisa Sequencial: valor {valorProcurado} encontrado na posição {posicaoSequencial}.");
+}
+else
+{
+    Console.WriteLine($"\nPesquisa Sequencial: valor {valorProcurado} não encontrado.");
+}
+static int PesquisaSequencial(int[] vetor, int valor)
+{
+    for (int i = 0; i < vetor.Length; i++)
+    {
+        if (vetor[i] == valor)
+            return i; //Retorna o índice do valor encontrado
+    }
+    return -1; // Retorna -1 se não encontrar
+}
